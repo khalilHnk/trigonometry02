1f63420d4fc08e8d972e9658277f81665692ef67
khalilHnk <79371578+khalilHnk@users.noreply.github.com>
2024-08-24 22:57:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
+'use strict'
 console.log('main:debut');
+function checkElement(x, w) {
+  return (x instanceof HTMLElement && x.offsetWidth === w && x.offsetHeight === x.offsetWidth);
+}
+(function main(R = 200, MW = 250){
+  if (typeof window !== 'object' || window.top !== window.self) {
+    console.warn('window:failed');
+    return;
+  }
+  window.addEventListener(
+    'load',
+    () => {
+      try {
+        const c = document.getElementById('box')?.querySelector('canvas')?.getContext('2d');
+        const p = document.getElementById('box')?.querySelector('#plan');
+        if (!(
+          c instanceof CanvasRenderingContext2D && checkElement(c.canvas, 2 * MW)
+          && checkElement(p, 2 * R)
+        ))
+        {
+          throw new Error('elements:failed');
+        }
+
+        console.log('%celements:success','color:blue');
+        
+        
+      } catch(err) {
+        console.error(err.message);
+      }
+    },
+    {
+      once: true
+    }
+  );
+})();
+console.log('main:fin');
